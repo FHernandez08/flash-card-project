9363e9bcb59828ad71890c83f7443d6e1f917da3
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-02-10 14:27:41 -0600

    Card flipping mechanisms

--- main.py
+++ main.py
@@ -7,36 +7,49 @@ BACKGROUND_COLOR = "#B1DDC6"
 TITLE_FONT = ("Ariel", 40, "italic")
 WORD_FONT = ("Ariel", 60, "bold")
 
-df = pd.read_csv("data/french_words.csv")
+data = pd.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
 
-dict_records = df.to_dict(orient="records")
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
 
-def random_word():
-    number = random.randint(0, 101)
-    word = dict_records[number]['French']
+    canvas.itemconfig(french_title, text="French", fill="black")
+    canvas.itemconfig(main_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=old_image)
+    flip_timer = window.after(3000, func=change_card)
 
-    canvas.itemconfig(french_title, text="French")
-    canvas.itemconfig(main_word, text=word)
+def change_card():
+    new_image = PhotoImage(file="images/card_back.png")
+
+    canvas.itemconfig(canvas_image, image=new_image)
+    canvas.itemconfig(french_title, fill='#FFFFFF', text="English")
+    canvas.itemconfig(main_word, fill='#FFFFFF', text=current_card["English"])
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=change_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-main_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=main_img)
+old_image = PhotoImage(file="images/card_front.png")
+canvas_image = canvas.create_image(400, 263, image=old_image)
 canvas.grid(row=0, column=0, columnspan=2)
 
-french_title = canvas.create_text(400, 150, text="Title", font=TITLE_FONT)
-main_word = canvas.create_text(400, 263, text="Word", font=WORD_FONT)
+french_title = canvas.create_text(400, 150, text="", font=TITLE_FONT)
+main_word = canvas.create_text(400, 263, text="", font=WORD_FONT)
 
 wrong_img = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0, command=random_word)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
 
 right_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_img, highlightthickness=0, command=random_word)
+right_button = Button(image=right_img, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
 
+next_card()
 
 window.mainloop()
